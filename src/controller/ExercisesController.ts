c578de5842d81d8c7b643b21689110d68b5eb3e4
RodolfoPedra <rodolfopedra@hotmail.com>
2020-07-08 00:17:43 -0400

    modificações ControllerExercises

--- src/controller/ExercisesController.ts
+++ src/controller/ExercisesController.ts
@@ -1,22 +1,46 @@
-import {getRepository} from "typeorm";
+import {getRepository, Repository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
 import Exercises from "../entity/Exercises";
+
 
 class ExercisesController {
 
     
-    async store(request: Request, response: Response, next: NextFunction) {
+    async create(req: Request, res: Response, next: NextFunction) {
         try {
-            const exercisesRepository = getRepository(Exercises);
-            const {name_exercise, description_exercise} = await exercisesRepository.save(request.body);
-            return response.status(201).json({
+            const repo = getRepository(Exercises);
+            const {name_exercise, description_exercise} = await repo.save(req.body);
+            return res.status(201).json({
                 name_exercise,
                 description_exercise
             })
         } catch (error) {
-            console.log('erro ao criar exercício: ', error);
-            
+            console.log('erro ao criar exercício: ', error);        
         }
+    }
+
+    async findAll(req: Request, res: Response, next: NextFunction) {
+        try {
+            const repo = getRepository(Exercises);
+            const data = await repo.find();
+
+            return res.status(200).json(data);
+        } catch (error) {
+            console.log('erro ao buscar exercícios: ', error);                    
+        }
+
+    }
+
+    async remove(req: Request, res: Response, next: NextFunction) {
+        try {
+            const repo = getRepository(Exercises);
+            const exercise = await repo.findOne(req.params.id);
+            await repo.remove(exercise);
+            return res.json({ok: "Dados removidos com sucesso"});
+        } catch (error) {
+            console.log('erro ao buscar exercícios: ', error);                    
+        }
+
     }
 
     // async one(request: Request, response: Response, next: NextFunction) {
--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,9 @@ import ExercisesController from './controller/ExercisesController';
 
 const routes = Router();
 
-routes.post('/teste', ExercisesController.store);
-
+routes.get('/exercicio', ExercisesController.findAll);
+routes.post('/exercicio', ExercisesController.create);
+routes.delete('/exercicio/:id', ExercisesController.remove);
 
 export default routes;
 
